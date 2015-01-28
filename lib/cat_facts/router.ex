60ab7fe87fd51e39553ab6fa53540cad66a3ab0c
0chroma <21014+0chroma@users.noreply.github.com>
2015-01-28 00:14:41 -0800

    fixed hopefully

--- lib/cat_facts/router.ex
+++ lib/cat_facts/router.ex
@@ -9,34 +9,39 @@ defmodule CatFacts.Router do
 
   post "/" do
 
-    case HTTPoison.get "http://catfacts-api.appspot.com/api/facts" do
-      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-        {:ok, parsed} = JSON.decode body
-        slack_params = Map.get(conn, :params)
-        #url = "https://#{slack_params["team_domain"]}.slack.com/services/hooks/incoming-webhook?token=#{slack_params["token"]}"
-        url = System.get_env "SLACK_URL"
-        data = [
-          username: "cat-facts",
-          icon_emoji: ":cat:",
-          #channel: "#{slack_params["channel"]}",
-          text: hd parsed["facts"]
-        ]
-        IO.inspect slack_params
-        IO.inspect url
-        IO.inspect data
-        {:ok, data} = JSON.encode data
-        case HTTPoison.post url, data, %{"Content-type" => "application/json"} do
-          {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
-            send_resp(conn, 200, hd parsed["facts"])
-          err ->
-            Logger.error "Error from slack API", [err: err]
-            IO.inspect err
-            send_resp(conn, 500, "Sorry! Cat facts are unavailable, try again later!")
-        end
-      err ->
-        Logger.error "Error from cat facts API", [err: err]
-        IO.inspect err
-        send_resp(conn, 500, "Sorry! Cat facts are unavailable, try again later!")
+    slack_params = Map.get(conn, :params)
+    if slack_params["channel_name"] == System.get_env("SLACK_CHANNEL") do
+      case HTTPoison.get "http://catfacts-api.appspot.com/api/facts" do
+        {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+          {:ok, parsed} = JSON.decode body
+          #url = "https://#{slack_params["team_domain"]}.slack.com/services/hooks/incoming-webhook?token=#{slack_params["token"]}"
+          url = System.get_env "SLACK_URL"
+          data = [
+            username: "cat-facts",
+            icon_emoji: ":cat:",
+            #channel: "##{slack_params["channel_name"]}",
+            text: hd parsed["facts"]
+          ]
+          #IO.inspect slack_params
+          #IO.inspect url
+          #IO.inspect data
+          {:ok, data} = JSON.encode data
+          case HTTPoison.post url, data, %{"Content-type" => "application/json"} do
+            {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+              #send_resp(conn, 200, hd parsed["facts"])
+              send_resp(conn, 200, "")
+            err ->
+              Logger.error "Error from slack API", [err: err]
+              IO.inspect err
+              send_resp(conn, 500, "Sorry! Cat facts are unavailable, try again later!")
+          end
+        err ->
+          Logger.error "Error from cat facts API", [err: err]
+          IO.inspect err
+          send_resp(conn, 500, "Sorry! Cat facts are unavailable, try again later!")
+      end
+    else
+      send_resp(conn, 500, "Sorry! Cat facts are only available in #{System.get_env "SLACK_CHANNEL"}.")
     end
   end
 
