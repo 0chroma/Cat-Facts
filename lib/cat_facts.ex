17b6546bc44837ed96f464411042a3ee825e7e09
0chroma <21014+0chroma@users.noreply.github.com>
2015-01-27 21:46:04 -0800

    added port env var for heroku

--- lib/cat_facts.ex
+++ lib/cat_facts.ex
@@ -3,6 +3,9 @@ require Logger
 defmodule CatFacts do
   def start(_type, _args) do
     opts  = [port: 4000, compress: true, linger: {true, 10}]
+    if port = System.get_env("PORT") do
+      opts = Keyword.put(opts, :port, String.to_integer(port))
+    end
     Logger.info "Starting Cowboy on port #{opts[:port]}"
     Plug.Adapters.Cowboy.http CatFacts.Router, [], opts
     #CatFacts.Supervisor.start_link
